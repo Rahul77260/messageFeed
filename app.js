447ba82105d7be7064e3c01d49f939701873ed42
Rahul <rahul.kumar@collpoll.com>
2023-06-26 10:04:00 +0530

    added timing functionality

--- app.js
+++ app.js
@@ -21,14 +21,16 @@ myApp.config(['$routeProvider', function($routeProvider){
       });
 }]);
 
-myApp.controller('myCtr',function($scope){
+myApp.controller('myCtr',function($scope, $filter,$interval){
     $scope.id=0;
     $scope.show=false;
     $scope.allPosts=[{
-      name: "ajay devgan",
+      name: "Varun",
       messg:"PM Modi meets Elon Musk, discuss Tesla, Starlink India plans: All you need to know",
+      posted:new Date(),
       likes:56,
       dislike: 4,
+    
       comments: [{
         content: "30-minute intervals",
         editMode:false},
@@ -37,10 +39,12 @@ myApp.controller('myCtr',function($scope){
         editMode: false }]
     },
   {
-    name:"deepika padukone",
+    name:"Saksham",
     messg:"India slams China for blocking proposal to designate 26/11 planner",
+    posted:new Date(),
     likes:3,
     dislike: 43,
+
     comments: [{
       content:"Internal US Department",
       editmode:false
@@ -55,9 +59,10 @@ myApp.controller('myCtr',function($scope){
       ]
   },
 {
-  name: "akshay kumar",
-  messg: "submarine carrying five crew members disappeared on Sunday, American media is reporting. Internal US Department of Homeland Security memos say the banging sounds could be heard for hours at 30-minute intervals.",
+  name: "Rahul",
+  messg: "submarine carrying five crew members disappeared on Sunday, American media is reporting. Internal US Department",
   likes: 10000,
+  posted:new Date(),
   dislike: 122,
   comments: [{
     content: "🥳🥳🥳🥳🥳🥳🥳🥳",
@@ -71,7 +76,7 @@ myApp.controller('myCtr',function($scope){
     $scope.newComment = ''; // Initialize the new comment input
     $scope.showCommentInput = false; // Set initial state to show the comment input field
    
-    $scope.Edit = function() {
+    $scope.Edit = () =>{
       if ($scope.editMode) {
         // Save changes
         $scope.editMode = false;
@@ -80,14 +85,26 @@ myApp.controller('myCtr',function($scope){
         $scope.editMode = true;
       }
     };
-    $scope.setId = function (singlePost){
+    const updateTime = () => {
+      let post = [];
+      $scope.allPosts.forEach((singlePost) => {
+        // console.log("SinglePost",singlePost);
+        post.posted = $filter('timeAgo')(singlePost);
+        // console.log("posted",singlePost.posted);
+      });
+    }
+    
+  
+    // Update the time every second
+    $interval(updateTime, 1000);
+    $scope.setId =  (singlePost)=>{
       console.log('====singlePost: ', singlePost);
       let id=$scope.allPosts.indexOf(singlePost);
       console.log('singlePost: ', singlePost);
       $scope.id=id;
     }
     // adding a new content in feed
-    $scope.addToFeed = function() {
+    $scope.addToFeed = ()=> {
       
         $scope.show=false;
         $scope.allPosts.push({
@@ -95,6 +112,7 @@ myApp.controller('myCtr',function($scope){
           messg:$scope.story,
           likes:0,
           dislike: 0,
+          posted:new Date(),
           comments: []
         });
         $scope.story="";
@@ -102,7 +120,7 @@ myApp.controller('myCtr',function($scope){
         console.log("===",$scope.allPosts);
     };
     //likes
-    $scope.incrementLike = function(singlePost){
+    $scope.incrementLike = (singlePost)=>{
       console.log('singlePost: ', singlePost);
       console.log("===",$scope.allPosts);
     //  let indexx= $scope.allPosts.indexOf(singlePost);
@@ -112,7 +130,7 @@ myApp.controller('myCtr',function($scope){
       $scope.allPosts[indexx].likes+=1;
     };
     //dislike
-    $scope.decrementLike = function(singlePost){
+    $scope.decrementLike = (singlePost)=>{
       let indexx = $scope.allPosts.indexOf(singlePost);
       $scope.allPosts[indexx].dislike+=1;
       console.log($scope.allPosts);
@@ -121,7 +139,7 @@ myApp.controller('myCtr',function($scope){
       $scope.show = true;
     };
 //add comments
-  $scope.addComment = function (singlePost) {
+  $scope.addComment =  (singlePost) =>{
     console.log('singlePost: ', singlePost);
     console.log("========",$scope.allPosts);
     $scope.showCommentInput = false;
@@ -134,42 +152,48 @@ myApp.controller('myCtr',function($scope){
     }
   };
   //save changes to edit content feed
-  $scope.saveChanges = function () {
+  $scope.saveChanges =  ()=> {
     console.log("+++=",$scope.editedContent);
-    $scope.allPosts[$scope.id].messg = $scope.editedContent;
-    $scope.editMode = false;
+    if($scope.editedContent===undefined){
+        $scope.allPosts[$scope.id].messg = $scope.allPosts[$scope.id].messg;
+      }
+      else{
+        $scope.allPosts[$scope.id].messg=$scope.editedContent;
+      }
+      $scope.editMode = false;
+   
   };
   //delete a feed
-  $scope.deleteFeed = function(singlePost){
+  $scope.deleteFeed = (singlePost)=>{
     console.log("+++++++",$scope.allPosts);
     let indexx = $scope.allPosts.indexOf(singlePost);
     $scope.allPosts.splice(indexx, 1);
     console.log("------",$scope.allPosts);
   }
   //edit comments
-  $scope.EditComment = function (singlePost,singleComment) {
+  $scope.EditComment =(singlePost,singleComment)=> {
     singleComment.editMode=true;
     singleComment.editedComment=singleComment.content;
   };
   //save comments
-  $scope.saveComment= function(singlePost,singleComment){
+  $scope.saveComment= (singlePost,singleComment)=>{
     singleComment.editMode=false;
     singleComment.content = singleComment.editedComment;
   }
   
   //cancel comments
-  $scope.cancelComment = function (){
+  $scope.cancelComment =  ()=>{
     singleComment.editMode=false;
   }
   //delete comment
-   $scope.deleteComment = function(singlePost,singleComment) {
+   $scope.deleteComment =(singlePost,singleComment)=> {
     var index = $scope.allPosts[singlePost].comments.indexOf(singleComment);
     console.log('index: ', index);
     if (index > -1) {
       $scope.allPosts[singlePost].comments.splice(index, 1); // Remove the comment from the comments array
     } 
   };
-  $scope.cancelAddComment = function (singlePost) {
+  $scope.cancelAddComment =  (singlePost)=> {
     showCommentInput = false;
     singlePost.val=''; 
   }
@@ -181,12 +205,12 @@ $scope.CancelFeed=()=>{
 }
 
   //save comments
-  $scope.Update=function(singleComment){
+  $scope.Update=(singleComment)=>{
     singleComment.editMode=false;
     singleComment.content= singleComment.editedComment;
    
   }
-  $scope.Cancel=function(singleComment){
+  $scope.Cancel=(singleComment)=>{
     singleComment.editMode=false;
      $scope.story="";
         $scope.userName="";
@@ -195,17 +219,64 @@ $scope.CancelFeed=()=>{
   }
   
 
-})
+});
 
-myApp.directive('postComponent', function() {
-  return {
-    restrict: 'EAC',
-    scope: {
-      singlePost: '=' 
-      // Two-way binding for the data attribute
-    },
-    templateUrl: 'postComponent.html',
-    controller: 'myCtr',
+// myApp.directive('postComponent', function() {
+//   return {
+//     restrict: 'EAC',
+//     scope: {
+//       singlePost: '=' 
+//       // Two-way binding for the data attribute
+//     },
+//     templateUrl: 'postComponent.html',
+//     controller: 'myCtr',
+//   };
+// });
+myApp.filter('timeAgo', ()=> {
+  return function(singlePost) {
+    console.log("postedtime",singlePost.posted);
+   let postTime=singlePost.posted
+  
+    let current = new Date();
+     console.log("current",current);
+    //  let previous = input;
+    // console.log("previous",previous);
+   
+    let elapsed = current - postTime;
+
+    console.log("elapsed",elapsed);
+
+    let seconds = Math.floor(elapsed / 1000);
+    if (seconds < 60) {
+      return "just now";
+    }
+
+    let minutes = Math.floor(seconds / 60);
+    if (minutes < 60 ) {
+      return minutes + " minute ago";
+    }
+
+    let hours = Math.floor(minutes / 60);
+    if (hours < 24) {
+      return hours + " hour ago";
+    }
+
+    let days = Math.floor(hours / 24);
+    if (days < 30) {
+      return days + " day ago";
+    }
+
+    let months = Math.floor(days / 30);
+    if (months < 12) {
+      return months + " month ago";
+    }
+   else{
+    let years = Math.floor(months / 12);
+    return years + " year ago";
+   }
   };
 });
+
+
+
  
